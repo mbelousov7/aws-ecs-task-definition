a01b9b8017b7c8df84926777b9c515e108abb966
Mikhail Belousov <mikhail.belousov@lseg.com>
2023-03-31 16:04:36 +0200

    1.0.3

--- output.tf
+++ output.tf
@@ -8,7 +8,22 @@ output "task_definition_arn" {
   description = "created task definition arn"
 }
 
+output "task_definition_family" {
+  value       = aws_ecs_task_definition.default.family
+  description = "created task family name"
+}
+
+output "container_definition_name" {
+  value       = local.container_definition.name
+  description = "created container definition name"
+}
+
 output "aws_cloudwatch_log_group_arn" {
   value       = aws_cloudwatch_log_group.default.arn
   description = "created cloudwatch log group arn"
+}
+
+output "aws_cloudwatch_log_group_name" {
+  value       = aws_cloudwatch_log_group.default.name
+  description = "created cloudwatch log group name"
 }
